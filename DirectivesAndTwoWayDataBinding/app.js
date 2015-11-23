a47c92dc22aa9431cd6d0f455762049cd2a7a1d8
Matt Smith <msmith@itree.com.au>
2015-11-23 16:23:47 +1100

    Lecture 18: Directives and Two Way Data Binding

---
+++ DirectivesAndTwoWayDataBinding/app.js
@@ -0,0 +1,10 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
+    
+    $scope.handle = '';
+    $scope.lowercaseHandle = function() {
+        return $filter('lowercase')($scope.handle);
+    }
+    
+}]);
