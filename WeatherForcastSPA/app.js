6a652cec630f1739c61cf0d3a8745f3e9cddecab
Matthew Smith <mdsmith00@gmail.com>
2015-12-04 09:34:04 +1100

    Lecture 44: Binding Data - Part 1

--- WeatherForcastSPA/app.js
+++ WeatherForcastSPA/app.js
@@ -14,6 +14,10 @@ weatherApp.config(function ($routeProvider) {
         .when('/forcast', {
             templateUrl: 'pages/forcast.htm',
             controller: 'forcastController'
+        })
+        .when('/forcast/:days', {
+            templateUrl: 'pages/forcast.htm',
+            controller: 'forcastController'
         });
 });
 
@@ -36,8 +40,23 @@ weatherApp.controller('homeController', ['$scope', 'forcastService', function ($
     });
 }]);
 
-weatherApp.controller('forcastController', ['$scope', 'forcastService', function ($scope, forcastService) {
+weatherApp.controller('forcastController', ['$scope', 'forcastService', '$resource', '$routeParams', function ($scope, forcastService, $resource, $routeParams) {
     "use strict";
 
     $scope.city = forcastService.city;
+
+    $scope.days = $routeParams.days || 2;
+
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+
+    /*global weatherApi*/
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days, appid: "2de143494c0b295cca9337e1e96b00e0", units: "metric" });
+
+    $scope.convertToFahrenheit = function (degK) {
+        return Math.round((1.8 * (degK - 273)) + 32);
+    };
+
+    $scope.convertToDate = function (milliseconds) {
+        return new Date(milliseconds * 1000);
+    };
 }]);
